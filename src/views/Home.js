130ba0cbd897442697e662b4342f1805d10fde7d
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-02-14 17:47:51 -0500

    SPA views created -Home, Cards, Error views created -Footer component created -index.html: script source changed to index.js

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,15 +2,14 @@ import { Footer } from "./../components/Footer.js";
 
 export const Home = () => {
     const section = document.createElement("section");
-        const div = document.createElement("div");
-        div.innerHTML =  `        
-            <h1>WELCOME TO CRUISE LINES</h1>
-            <img src="adventurOfTheSeasAruba.jpeg"/>
-            <p>Please press the GO button and enjoy the Trip</p>
-            <button>GO</button>
-        `;
+    const div = document.createElement("div");
+    div.innerHTML =  `        
+        <h1>WELCOME TO CRUISE LINES</h1>
+        <img src="adventurOfTheSeasAruba.jpeg"/>
+        <p>Please press the GO button and enjoy the Trip</p>
+        <button>GO</button>
+    `;
     section.append(div, Footer());
     return section;
 };
 
-// export default Home;
