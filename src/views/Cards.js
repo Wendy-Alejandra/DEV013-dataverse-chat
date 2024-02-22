7a276d7c326e706a0d03d9b222b4b27f6b76720b
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-02-22 16:25:44 -0500

    Filters applied to the cards

--- src/views/Cards.js
+++ src/views/Cards.js
@@ -1,50 +1,25 @@
 import { data } from "./../data/dataset.js";
 import { Header } from "./../components/Header.js";
 import { Menu } from "./../components/Menu.js";
+import { CardsRender } from "./../components/CardsRender.js"
 import { Footer } from "./../components/Footer.js";
-import { navigateTo } from "../router.js";
 import { filterData } from "./../lib/dataFunctions.js";
 
 export const Cards = () => {
+  const section = document.createElement("section");
   const div = document.createElement("div");
-  const ul= document.createElement("ul");
-  let html = "";
-  data.forEach(card => {
-    html += `<li itemscope="" itemtype="name" class="cards">
-    <img itemprop="image" src="${card.imageUrl}" alt="${card.name}">
-    <dl itemprop="name">${card.name}</dl>
-    <dd itemprop="departureBoatTerminal"> Port: ${card.facts.departurePort}</dd>
-    <dd itemprop="totalPrice">${card.facts.cruisePrice}</dd>
-    <dd itemprop="description">AVG PER PERSON</dd>
-    <button class="see-more" data-id="${card.id}">See more</button>
-</li>`
-  });
-  
-  ul.innerHTML = html;
-  div.append(Header(), Menu(), ul, Footer());
+  div.appendChild(CardsRender(data));
 
+  section.append(Header(), Menu(), div, Footer());
 
-  const selectFilter = div.querySelector('select[data-testid="select-filter"]');
-  console.log(selectFilter);
+  /* Filtering data (filtrando la data) */
+  const selectFilter = section.querySelector('select[data-testid="select-filter"]');
   selectFilter.addEventListener("change", (e)=>{
     const filteredData= filterData(data, "cruisePrice", e.target.value);
-    ul.textContent="";
-    ul.append(filteredData);
-    console.log(ul);
+    div.textContent="";
+    div.append(CardsRender(filteredData));
   });
 
-  const botones = ul.querySelectorAll(".see-more");
-  console.log(botones);
-  botones.forEach((boton) => {
-    boton.addEventListener("click", () => {
-      const cardId = boton.getAttribute("data-id");
-      console.log(cardId);
-      navigateTo("/moreInfoCards", cardId);
-      console.log(navigateTo)
-    });
-  });
-  
-  return div;
-
+  return section;
 };
 
