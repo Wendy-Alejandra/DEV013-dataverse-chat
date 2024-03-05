3552a727eab8d3c7850e30716adc532318d02b2c
mcarrobo24 <mcaf.dev24@gmail.com>
2024-03-05 16:29:18 -0500

    Communicate OpenAIApi

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,9 +1,35 @@
 // Importa la función para obtener la API KEY desde apiStorage.js
 import { getApiKey } from "./apiStorage.js";
 
+// obtener la apikey
+const receiveApiKey = getApiKey();
 
-export const communicateWithOpenAI = (messages) => {
+
+export const communicateWithOpenAI = async (cruiseShips, userMessages) => {
 //Aquí es donde debes implementar la petición con fetch o axios
-  messages = getApiKey();
-  return messages;
+  const respuesta = await fetch (`https://api.openai.com/v1/chat/completions`, {
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json",
+      Authorization: "Bearer" + receiveApiKey,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          "role": "system",
+          "content": `You are a cruise ship: ${cruiseShips}, give short answers`,
+        },
+        {
+          "role": "user",
+          "content": userMessages,
+        },
+      ]
+    })
+  });
+  console.log (respuesta);
+
+
+  return respuesta;
 };
+
