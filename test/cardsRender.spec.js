262a7cafa5f51d1837250feb7843fffb37af0366
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-03-18 14:27:46 -0500

    cardsRender.spec.js tests up and running

--- test/cardsRender.spec.js
+++ test/cardsRender.spec.js
@@ -1,52 +1,33 @@
-// import { CardsRender } from "./../src/components/CardsRender.js";
+import { CardsRender } from "./../src/components/CardsRender.js";
+import { navigateTo } from "./../src/router.js";
+import { data as fakeData } from "./data.js";
 
-// // Mock de la función navigateTo
-// jest.mock("../router.js", () => ({
-//   navigateTo: jest.fn(),
-// }));
+// Mock de la función navigateTo
+jest.mock("./../src/router.js", () => ({
+  navigateTo: jest.fn(),
+}));
 
-// describe("CardsRender component", () => {
-//   test("CardsRender renders correctly", () => {
-//     const testData = [
-//       {
-//         id: 1,
-//         imageUrl: "url1",
-//         name: "Card 1",
-//         facts: {
-//           departurePort: "Port 1",
-//           cruisePrice: "$100",
-//         },
-//       },
-//       {
-//         id: 2,
-//         imageUrl: "url2",
-//         name: "Card 2",
-//         facts: {
-//           departurePort: "Port 2",
-//           cruisePrice: "$200",
-//         },
-//       },
-//     ];
+describe("CardsRender component", () => {
+  it("CardsRender renders correctly", () => {
+    // Renderizar el componente CardsRender con datos de prueba
+    const ulElement = CardsRender(fakeData);
 
-//     // Renderizar el componente CardsRender con datos de prueba
-//     const ulElement = CardsRender(testData);
+    // Verificar que el elemento sea un objeto de tipo HTMLUListElement
+    expect(ulElement instanceof HTMLUListElement).toBe(true);
 
-//     // Verificar que el elemento sea un objeto de tipo HTMLUListElement
-//     expect(ulElement instanceof HTMLUListElement).toBe(true);
+    // Verificar que se han renderizado las tarjetas correctamente
+    expect(ulElement.querySelectorAll(".cards").length).toBe(fakeData.length);
 
-//     // Verificar que se han renderizado las tarjetas correctamente
-//     expect(ulElement.querySelectorAll(".cards").length).toBe(testData.length);
-
-//     // Verificar que los botones 'See more' tienen los atributos y eventos esperados
-//     const buttons = ulElement.querySelectorAll(".see-more");
-//     buttons.forEach((button, index) => {
-//       expect(button.getAttribute("data-id")).toBe(
-//         testData[index].id.toString()
-//       );
-//       button.click();
-//       expect(navigateTo).toHaveBeenCalledWith("/moreInfoCards", {
-//         id: testData[index].id,
-//       });
-//     });
-//   });
-// });
+    // Verificar que los botones 'See more' tienen los atributos y eventos esperados
+    const buttons = ulElement.querySelectorAll(".see-more");
+    buttons.forEach((button, index) => {
+      expect(button.getAttribute("data-id")).toBe(
+        fakeData[index].id.toString()
+      );
+      button.click();
+      expect(navigateTo).toHaveBeenCalledWith("/moreInfoCards", {
+        id: fakeData[index].id.toString(),
+      });
+    });
+  });
+});
